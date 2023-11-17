a8421319929b8768167a06478d720ca21d9c4381
Joseph Colon <joseph_frito13@yahoo.com>
2023-11-16 19:26:34 -0500

    fixed bug when selecting only one prompt

--- develop/script.js
+++ develop/script.js
@@ -1,23 +1,24 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
+//All vars needed for password generation
 var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',];
 var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',];
 var numbers = ['1','2','3','4','5','6','7','8','9','0',];
 var specialChar = ['!','@','#','$','%','^','&','*','(',')','<','>','?','/','|',];
 
-var characterLength = 8;
+var characterLength = 128;
 var choiceArr = []
 
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
   var correctPrompts =  getPrompts();
   var passwordText = document.querySelector("#password");
-
+// If promts are true then it would generate password
   if (correctPrompts) {
       var finalPassword = generatePassword();  
       passwordText.value = finalPassword;
@@ -55,14 +56,12 @@ function getPrompts(){
         choiceArr = choiceArr.concat(lowerCase);
     }
 
+    if (confirm("would you like Numbers in your password?")) {
+        choiceArr = choiceArr.concat(numbers);
+    }
+
     if (confirm("would you like special characters in your password?")) {
         choiceArr = choiceArr.concat(specialChar);
     }
-
-    if (confirm("would you like Numbers in your password?")) {
-        choiceArr = choiceArr.concat(numbers);
-    } else {
-        window.alert("Please choose at least one option");
-      }
     return true;
 }
